4c04c7eb5e079cc64b3008e1b7c298b6e6e2246b
cramster <87053198+Cramster@users.noreply.github.com>
2024-03-19 14:49:11 -0400

    intellij product service/repo query add

--- src/main/java/org/example/Repository/ProductRepository.java
+++ src/main/java/org/example/Repository/ProductRepository.java
@@ -1,7 +1,13 @@
 package org.example.Repository;
+import java.util.List;
+
 import org.example.Entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
+    @Query("From Product where productTitle =: productTitle")
+    List<Product> findByTitle(@Param("productTitle") String productTitle);
 }
