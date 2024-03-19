3e065a5a4a5789d610fcfdc0332478f36ed92afd
cramster <87053198+Cramster@users.noreply.github.com>
2024-03-19 11:56:15 -0400

    product/seller stubs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,12 @@
 package org.example;
-import java.sql.Connection;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-
+        ApplicationContext context = SpringApplication.run(Main.class);
     }
 }
