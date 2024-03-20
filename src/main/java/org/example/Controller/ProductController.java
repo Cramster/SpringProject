c7fd0b597811c6f618042a41c6ce289e53949fe4
Sblalock0806 <steven.blalock0217@gmail.com>
2024-03-20 07:39:29 -0400

    update to product post

--- src/main/java/org/example/Controller/ProductController.java
+++ src/main/java/org/example/Controller/ProductController.java
@@ -34,9 +34,15 @@ public class ProductController {
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
+//    @PostMapping("seller/{sellerId}/product")
+//    public ResponseEntity<Product> addProduct(@RequestBody Product p, @PathVariable int productId) throws Exception {
+//        Product product = productService.saveProduct(productId, p);
+//        return new ResponseEntity<>(product, HttpStatus.CREATED);
+//    }
+
     @PostMapping("seller/{sellerId}/product")
-    public ResponseEntity<Product> addProduct(@RequestBody Product p, @PathVariable int productId) throws Exception {
-        Product product = productService.saveProduct(productId, p);
+    public ResponseEntity<Product> addProduct(@RequestBody Product p, @PathVariable int sellerId) throws Exception {
+        Product product = productService.saveProduct(sellerId, p);
         return new ResponseEntity<>(product, HttpStatus.CREATED);
     }
 
